fb839514c2141a93199fe91eb898c5322bc7ae7b
Payal Gaikwad <payal.72445@gmail.com>
2026-01-17 17:24:15 +0530

    Update athena_queries.sql

--- sql_queries/athena_queries.sql
+++ sql_queries/athena_queries.sql
@@ -16,3 +16,45 @@ SELECT product, SUM(sales) AS total_sales
 FROM raw_data
 GROUP BY product
 ORDER BY total_sales DESC;
+
+🔹 1. Total Revenue & Profit (Executive KPI)
+SELECT 
+    SUM(sales) AS total_revenue,
+    SUM(profit) AS total_profit
+FROM raw_data;
+
+
+🔹 2. Monthly Sales Trend (Time Series)
+SELECT 
+    date_format(order_date, '%Y-%m') AS month,
+    SUM(sales) AS monthly_sales
+FROM raw_data
+GROUP BY date_format(order_date, '%Y-%m')
+ORDER BY month;
+
+3. Region Contribution Percentage
+SELECT 
+    region,
+    SUM(sales) AS region_sales,
+    ROUND(
+        SUM(sales) * 100.0 / (SELECT SUM(sales) FROM raw_data),
+        2
+    ) AS sales_percentage
+FROM raw_data
+GROUP BY region;
+
+4. Category Performance Ranking
+SELECT 
+    category,
+    SUM(sales) AS total_sales,
+    RANK() OVER (ORDER BY SUM(sales) DESC) AS sales_rank
+FROM raw_data
+GROUP BY category;
+
+🔹 5. Top 3 Products by Profit
+SELECT 
+    product,
+    SUM(profit) AS total_profit
+FROM raw_data
+GROUP BY product
+ORDER BY tot
